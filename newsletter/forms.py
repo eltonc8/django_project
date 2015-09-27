0850de591783e7ca9c52324babb588c73996799e
eltonc88 <eltonchan@cal.berkeley.edu>
2015-09-26 22:03:47 -0700

    add validations

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -6,3 +6,11 @@ class SignUpForm(forms.ModelForm):
     class Meta:
         model = SignUp
         fields = ['full_name', 'email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_base, provider = email.split('@')
+        domain, extension = provider.split('.')
+        if not extension == "edu":
+            raise forms.ValidationError("Please use a valid .EDU email address")
+        return email
