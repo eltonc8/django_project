98268af803031a3e57d4eeead876f4a5126eb31b
eltonc88 <eltonchan@cal.berkeley.edu>
2015-09-26 23:11:43 -0700

    add logic surrounding post/get request around form

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 
+from .forms import SignUpForm
+
 # Create your views here.
 def home(request):
     title = 'Welcome '
-    if request.user.is_authenticated():
-        title = "My Title %s" %(request.user)
+    # if request.user.is_authenticated():
+    #     title = "My Title %s" %(request.user)
+    # if request.method == "POST":
+    #     print request.POST
+    form = SignUpForm(request.POST or None)
     context = {
         "template_title": title,
+        "form": form,
     }
+
+    if form.is_valid():
+        # form.save()
+        instance = form.save(commit=False)
+        print request.POST['email']
+        full_name = form.cleaned_data.get("full_name")
+        if not full_name:
+            full_name = "New full name"
+        instance.full_name = full_name
+        # if not instance.full_name == None:
+        #     instance.full_name = "Justin"
+        instance.save()
+        context = {
+            "title": "Thank you"
+        }
+
     return render(request, "home.html", context)
